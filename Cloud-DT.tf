0ed72e469e015689639141f4c9db39537c459a53
Dylan <dylan31400@outlook.com>
2024-12-24 00:09:12 +0100

    Update Cloud-DT.tf for new configuration

--- Cloud-DT.tf
+++ Cloud-DT.tf
@@ -42,3 +42,22 @@ resource "aws_security_group" "example" {
     Name = "dt-security-group"
   }
 }
+
+data "aws_ami" "ecs_optimized_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-ecs-hvm-2.0.202*-x86_64-ebs"]
+  }
+}
+
+resource "aws_instance" "ecs_instance" {
+  ami           = data.aws_ami.ecs_optimized_ami.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "DT-EC2"
+  }
+}
